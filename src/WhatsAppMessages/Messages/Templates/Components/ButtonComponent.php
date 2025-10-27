ce5b13a771b368811e61b0cbeb65c3b4bed33882
Wilbert Fernando Vela Rodríguez <wolfernand@gmail.com>
2025-10-27 12:59:14 -0600

    feat(ButtonComponent): Enhance button creation with URL subtype support
    - Added support for URL subtype in ButtonComponent.
    - Updated constructor and create method to accept subtype parameter.
    - Modified example in README to demonstrate new URL button functionality.

--- src/WhatsAppMessages/Messages/Templates/Components/ButtonComponent.php
+++ src/WhatsAppMessages/Messages/Templates/Components/ButtonComponent.php
@@ -1,35 +1,34 @@
 <?php
-
 namespace Axolotesource\LaravelWhatsappApi\WhatsAppMessages\Messages\Templates\Components;
-
 
 use Axolotesource\LaravelWhatsappApi\WhatsAppMessages\Constants\ComponentsType;
 use Exception;
 
 class ButtonComponent implements ComponentsInterface
 {
-    const SUB_TYPE_QUICK_REPLY = 'QUICK_REPLY';
-    const SUB_TYPE_URL = 'URL';
+    public const string SUB_TYPE_QUICK_REPLY = 'QUICK_REPLY';
+    public const string SUB_TYPE_URL = 'URL';
 
-    private string $subType = ButtonComponent::SUB_TYPE_QUICK_REPLY;
+    private string $subType;
     private int $index;
     private array $component;
 
     /**
      * @param array $components
      */
-    private function __construct(array $components)
+    private function __construct(array $components, string $subType = self::SUB_TYPE_QUICK_REPLY)
     {
         $this->component = $components;
+        $this->subType = $subType;
     }
 
     /**
      * @param array $components
      * @return ButtonComponent
      */
-    static function create(array $components) : ButtonComponent
+    static function create(array $components, string $subType = self::SUB_TYPE_QUICK_REPLY): ButtonComponent
     {
-        return new ButtonComponent($components);
+        return new ButtonComponent($components, $subType);
     }
 
     /**
